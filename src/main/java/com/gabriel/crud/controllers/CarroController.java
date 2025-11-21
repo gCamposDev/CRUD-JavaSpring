e137e73d81ad93db5dc54c3292c3f985dfb403b3
Gabriel Campos <gcamposlive@gmail.com>
2025-11-21 11:17:34 -0300

    Método createCarro

--- src/main/java/com/gabriel/crud/controllers/CarroController.java
+++ src/main/java/com/gabriel/crud/controllers/CarroController.java
@@ -1,9 +1,22 @@
 package com.gabriel.crud.controllers;
 
+import com.gabriel.crud.entities.Carro;
+import com.gabriel.crud.services.CarroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/carros")
 public class CarroController {
+
+    @Autowired
+    private CarroService carroService;
+
+    @PostMapping
+    public Carro createCarro(@RequestBody Carro carro){
+        return carroService.createCarro(carro);
+    }
 }
--- src/main/java/com/gabriel/crud/services/CarroService.java
+++ src/main/java/com/gabriel/crud/services/CarroService.java
@@ -1,7 +1,17 @@
 package com.gabriel.crud.services;
 
+import com.gabriel.crud.entities.Carro;
+import com.gabriel.crud.repositories.CarroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CarroService {
+
+    @Autowired
+    private CarroRepository carroRepository;
+
+    public Carro createCarro(Carro carro){
+        return carroRepository.save(carro);
+    }
 }
